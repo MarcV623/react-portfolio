9a7d92486d413068000cce9309118081dfbb36c2
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-10-25 12:44:58 -0500

    added second  project content to portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,5 @@
 import pictureone from '../assets/coding3.jpeg'
-
+import picturetwo from '../assets/code.jpeg'
 export default function Contact() {
     return (
       <div>
@@ -16,10 +16,10 @@ export default function Contact() {
 
           {/* Second project */}
           <div className="content-container">
-            <a href="https://marcv623.github.io/html-css-challenge/" target="_blank" rel="noreferrer">
-              <img src={pictureone} alt="some picture" width={500} height={300}></img>
+            <a href="https://marcv623.github.io/password-gen" target="_blank" rel="noreferrer">
+              <img src={picturetwo} alt="some picture" width={500} height={300}></img>
             </a>
-            <p><a href="https://google.com" target="_blank" rel="noreferrer">View Source Code!</a></p>
+            <p><a href="https://github.com/MarcV623/password-gen" target="_blank" rel="noreferrer">View Source Code!</a></p>
           </div>
           
           <p className="spacer"></p>

690bc4381bba99cd3b3a1126af1eed53b22a846b
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-10-25 13:33:05 -0500

    added sixth project content to portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,9 @@
 import pictureone from '../assets/coding3.jpeg'
 import picturetwo from '../assets/code.jpeg'
 import picturethree from '../assets/better-programmer.webp'
-
+import picturefour from '../assets/software-developement.webp'
+import picturefive from '../assets/computer_programming.jpeg'
+import picturesix from '../assets/coding-screen.jpeg'
 export default function Portfolio() {
     return (
       <div>
@@ -36,28 +38,28 @@ export default function Portfolio() {
 
           {/* Fourth project */}
           <div className="content-container">
-            <a href="https://marcv623.github.io/html-css-challenge/" target="_blank" rel="noreferrer">
-              <img src={pictureone} alt="some picture" width={500} height={300}></img>
+            <a href="https://warm-thicket-35521-50b83e044954.herokuapp.com/" target="_blank" rel="noreferrer">
+              <img src={picturefour} alt="some picture" width={500} height={300}></img>
             </a>
-            <p><a href="https://google.com" target="_blank" rel="noreferrer">View Source Code!</a></p>
+            <p><a href="https://github.com/MarcV623/Note-Taker" target="_blank" rel="noreferrer">View Source Code!</a></p>
           </div>
 
           <p className="spacer"></p>
 
           {/* Fifth project */}
           <div className="content-container">
-            <a href="https://marcv623.github.io/html-css-challenge/" target="_blank" rel="noreferrer">
-              <img src={pictureone} alt="some picture" width={500} height={300}></img>
+            <a href="https://marcv623.github.io/code-quiz/" target="_blank" rel="noreferrer">
+              <img src={picturefive} alt="some picture" width={500} height={300}></img>
             </a>
-            <p><a href="https://google.com" target="_blank" rel="noreferrer">View Source Code!</a></p>
+            <p><a href="https://github.com/MarcV623/code-quiz" target="_blank" rel="noreferrer">View Source Code!</a></p>
           </div>
 
           {/* Sixth project */}
           <div className="content-container">
-            <a href="https://marcv623.github.io/html-css-challenge/" target="_blank" rel="noreferrer">
-              <img src={pictureone} alt="some picture" width={500} height={300}></img>
+            <a href="https://marcv623.github.io/portfolio/" target="_blank" rel="noreferrer">
+              <img src={picturesix} alt="some picture" width={500} height={300}></img>
             </a>
-            <p><a href="https://google.com" target="_blank" rel="noreferrer">View Source Code!</a></p>
+            <p><a href="https://github.com/MarcV623/portfolio" target="_blank" rel="noreferrer">View Source Code!</a></p>
           </div>
 
           <p className="spacer"></p>
